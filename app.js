52b092a00516f703679c401ccbdc237c971115a7
s4masuch <98272740+s4masuch@users.noreply.github.com>
2023-09-04 21:25:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+const express = require('express');
 const http = require('http');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
 
 const hostname = '0.0.0.0'; // Listen on all available network interfaces
 const port = process.env.PORT || 8080; // Use the PORT environment variable or default to 8080
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 server.listen(port, hostname, () => {
