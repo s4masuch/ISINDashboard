14d332fe76d5892394b109844f549d18416bf608
s4masuch <98272740+s4masuch@users.noreply.github.com>
2023-09-04 20:11:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 8080;
+const hostname = '0.0.0.0'; // Listen on all available network interfaces
+const port = process.env.PORT || 8080; // Use the PORT environment variable or default to 8080
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
