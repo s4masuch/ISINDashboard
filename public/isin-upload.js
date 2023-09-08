3f40d67027850e484fc6ff9a64c7f08d879cbdd7
s4masuch <98272740+s4masuch@users.noreply.github.com>
2023-09-08 20:17:51 +0200

    Update isin-upload.js

--- public/isin-upload.js
+++ public/isin-upload.js
@@ -1,1 +1,36 @@
+// Add event listener to the upload button
+document.addEventListener("DOMContentLoaded", function () {
+  const uploadButton = document.getElementById("uploadButton");
+  const fileInput = document.getElementById("fileInput");
 
+  uploadButton.addEventListener("click", function () {
+    // Check if a file is selected
+    if (fileInput.files.length === 0) {
+      alert("Please select a file to upload.");
+      return;
+    }
+
+    // You can handle the file upload here
+    // For example, you can use FormData to send the file to a server
+    const formData = new FormData();
+    formData.append("file", fileInput.files[0]);
+
+    // Send the formData to the server using fetch or XMLHttpRequest
+    // Example using fetch:
+    fetch("/upload-endpoint", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (response.ok) {
+          alert("File uploaded successfully!");
+        } else {
+          alert("File upload failed. Please try again.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("File upload failed. Please try again.");
+      });
+  });
+});
