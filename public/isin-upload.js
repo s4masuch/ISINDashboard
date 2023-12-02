00a9d60a0707a7b9456268face79ebd87a44bf09
s4masuch <98272740+s4masuch@users.noreply.github.com>
2023-12-02 15:30:15 +0100

    Update isin-upload.js

--- public/isin-upload.js
+++ public/isin-upload.js
@@ -1,15 +1,13 @@
 // isin-upload.js
-alert('DOMContentLoaded event listener added');
-console.log('DOMContentLoaded event listener added');
 document.addEventListener('DOMContentLoaded', function () {
   const uploadButton = document.getElementById('uploadButton');
   uploadButton.addEventListener('click', handleFileUpload);
 });
 
-console.log('handleFileUpload function called!');
 function handleFileUpload() {
   const fileInput = document.getElementById('fileInput');
   const feedbackText = document.getElementById('feedbackText');
+  const tableContainer = document.getElementById('tableContainer'); // Add this line
 
   const file = fileInput.files[0];
 
@@ -25,35 +23,14 @@ function handleFileUpload() {
     const isins = parseFileContent(content);
 
     // Display ISINs in the table
-    displayISINs(isins);
+    displayISINs(isins, tableContainer); // Pass the table container
     feedbackText.innerText = 'File uploaded successfully.';
   };
 
   reader.readAsText(file);
 }
 
-console.log('parseFileContent function called!');
-function parseFileContent(content) {
-  // Implement your logic to parse the file content and extract ISINs
-  // For example, if it's a CSV file, you can split by lines and commas
-
-  const lines = content.split('\n');
-  const isins = [];
-
-  for (const line of lines) {
-    const columns = line.split(',');
-    const isin = columns[0]; // Assuming ISIN is in the first column
-    if (isin) {
-      isins.push(isin.trim());
-    }
-  }
-
-  return isins;
-}
-
-console.log('displayISINs function called!');
-function displayISINs(isins) {
-  const feedbackContainer = document.getElementById('feedbackContainer');
+function displayISINs(isins, tableContainer) {
   const table = document.createElement('table');
   table.border = '1';
 
@@ -70,9 +47,9 @@ function displayISINs(isins) {
   }
 
   // Replace existing table (if any) with the new one
-  while (feedbackContainer.firstChild) {
-    feedbackContainer.removeChild(feedbackContainer.firstChild);
+  while (tableContainer.firstChild) {
+    tableContainer.removeChild(tableContainer.firstChild);
   }
 
-  feedbackContainer.appendChild(table);
+  tableContainer.appendChild(table);
 }
